37c13f8005ef481d6b90990be1149b17555c3916
nabby27 <ivancordoba77@gmail.com>
2023-03-04 19:16:37 +0100

    add chapter 3

---
+++ rust-chapter-03/variables/src/main.rs
@@ -0,0 +1,41 @@
+fn main() {
+    // mutable variable
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+    
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    
+    // shadow and scope block
+    let x = 5;
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+
+    // tuple
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {y}");
+
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
+
+    // array
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let b = [3; 5];
+
+    let first = a[0];
+    let second = a[1];
+
+}
